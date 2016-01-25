37593796aa52f71a4a717b00dc3ded7d9770a508
fujisyan <wfknaoya0731@gmail.com>
2016-01-25 23:06:33 +0900

    24時間の取引高を追加

--- lib/jp_bitboard/client.rb
+++ lib/jp_bitboard/client.rb
@@ -38,27 +38,27 @@ module JpBitboard
 
      def update_bitflyer
         data = get_json('https://api.bitflyer.jp/v1/ticker')
-        return {bid: data["best_bid"], ask: data["best_ask"], last_price: data["ltp"]}
+        return {bid: data["best_bid"], ask: data["best_ask"], last_price: data["ltp"], volume: data["volume_by_product"].to_i}
      end
 
      def update_zaif
         data = get_json('http://api.zaif.jp/api/1/ticker/btc_jpy')
-        return {bid: data["bid"], ask: data["ask"], last_price: data["last"]}
+        return {bid: data["bid"], ask: data["ask"], last_price: data["last"], volume: data["volume"].to_i}
      end
 
      def update_coincheck
         data = get_json('https://coincheck.jp/api/ticker')
-        return {bid: data["bid"], ask: data["ask"], last_price: data["last"]}
+        return {bid: data["bid"], ask: data["ask"], last_price: data["last"], volume: data["volume"].to_i}
      end
 
      def update_btcbox
         data = get_json('https://www.btcbox.co.jp/api/v1/ticker/')
-        return {bid: data["buy"], ask: data["sell"], last_price: data["last"]}
+        return {bid: data["buy"], ask: data["sell"], last_price: data["last"], volume: data["vol"].to_i}
      end
 
      def update_quoine
         data = get_json('https://api.quoine.com/products')
-        return {bid: data[2]["market_bid"], ask: data[2]["market_ask"], last_price: data[2]["last_traded_price"]}
+        return {bid: data[2]["market_bid"], ask: data[2]["market_ask"], last_price: data[2]["last_traded_price"], volume: data[2]["volume_24h"].to_i}
      end
   end
 end
